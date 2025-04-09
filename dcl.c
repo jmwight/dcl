3b3a490c1c53d54376cb0d9a81af442ce03bfb75
John Morgan Wight <john.morgan.wight@proton.me>
2025-04-09 02:17:44 -0400

    Added qualifier handling

--- dcl.c
+++ dcl.c
@@ -8,6 +8,8 @@
 void dcl(void);
 void dirdcl(void);
 int gettoken(void);
+int contains(void *el, void *a, int layers, size_t nmem, size_t width, int (*match)(void *, void *));
+int streq(char *s0, char **s1);
 
 char name[MAXTOKEN];		/* identifier name */
 char datatype[MAXTOKEN]; 	/* data type = char, int, etc. */
@@ -18,9 +20,20 @@ int main(void) /* convert declarations to words */
 	/* 1st token on line is the datatype */
 	while(gettoken() != EOF) 
 	{
-		while(tokentype == '\n')
+		datatype[0] = '\0'; /* clear string */
+		while(tokentype == '\n') /* skip over blank lines */
 			gettoken();
-		strcpy(datatype, token);
+		/* scan for possible qualifers */
+		static const char *qualifiers[] = {"const", "static", "unsigned", "signed", "volatile", "register", "auto"};
+		for( ; contains((void*) token, (void*) qualifiers, 1, 7, sizeof(char), (int (*)(void*, void*))(streq)); gettoken())
+		{
+			strcat(datatype, token);
+			datatype[strlen(datatype) + 1] = '\0';
+			datatype[strlen(datatype)] = ' ';
+		}
+
+
+		strcat(datatype, token);
 		out[0] = '\0';
 		dcl(); /* parse rest of line */
 		if(tokentype != '\n')
@@ -73,4 +86,48 @@ void dirdcl(void)
 			strcat(out, " of");
 		}
 
+}
+
+int contains(void *el, void *a, int layers, size_t nmem, size_t width, int (*eqcomp)(void *, void *))
+{
+	while(nmem-- > 0)
+	{
+		/* layers is a workaround. If it's pointer to an array of pointers to strings. An just a pointer to an array of ints.
+		 * It's a bit of a creative solution I think */
+		/* only one dereference to get to a data element */
+		if(layers == 0)
+		{
+			if((*eqcomp)(el, (void *)a+nmem*width))
+				return 1;
+		}
+		/* two layers of dereferencing to get to data element */
+		else if(layers == 1)
+		{
+			if((*eqcomp)(el, (void **)a+nmem*width))
+				return 1;
+		}
+		/* three layers of dereferencing to get to data element */
+		else if(layers == 2)
+		{
+			if((*eqcomp)(el, (void ***)a+nmem*width))
+				return 1;
+		}
+		else
+		{
+			printf("Error: invalid number of layers\n");
+			return 0;
+		}
+			
+
+	}
+	return 0;
+}
+
+/* streq: check if two strings are equal. If they are return 1, if not return 0 */
+int streq(char *s0, char **s1)
+{
+	if(strcmp(s0, *s1) == 0)
+		return 1;
+	else
+		return 0;
 }
